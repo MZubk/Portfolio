99f7cbb05eef092d2b17d5db003396b677510b54
Murillo Zubk <mzubk.dev@gmail.com>
2023-10-04 20:17:10 -0300

    atualizando as cores

--- src/components/Projects/index.jsx
+++ src/components/Projects/index.jsx
@@ -1,6 +1,6 @@
 export default function Projects() {
   return (
-    <div className="font-serif mb-5 text-center items-center bg-gradient-to-br from-gray-900 from-20% via-gray-800 to-slate-900 border_primary">
+    <div className="font-serif mb-5 text-center items-center bg-gradient-to-br from-gray-900 from-20% via-slate-800 to-gray-900 border_primary">
       <h3 className="text-xl sm:text-2xl md:text-3xl lg:text4xl text-center p-4">
         Projetos
       </h3>
